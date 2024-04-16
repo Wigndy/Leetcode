e7ca4a1b719df6e8fefc034eaea07ffcde30b2ad
Diep_Kim_Phong <159527565+Wigndy@users.noreply.github.com>
2024-04-16 10:24:50 +0700

    Time: 24 ms (79.31%), Space: 24.9 MB (51.66%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -14,4 +14,25 @@ public:
         
         return answer;
     }
+
+    /*
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, int> mp;
+        vector<vector<string>> ans; 
+
+        for (int i = 0; i < strs.size(); i++) {
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            if (mp.find(temp) != mp.end()) {
+                ans[mp[temp]].push_back(strs[i]);
+            } else {
+                mp[temp] = ans.size();
+                ans.push_back({strs[i]});
+                
+            }
+        }
+
+        return ans;
+    }
+    */ //chưa hiểu cách phân loại của thằng này lắm
 };

2fe6c364bc88492fa60fc6a73f15af4381888b54
Le_Nguyen_Kim_Khoi <159527565+Wigndy@users.noreply.github.com>
2024-10-01 14:20:07 +0700

    Time: 0 ms (100.00%), Space: 7.7 MB (99.39%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,24 +1,11 @@
 class Solution {
 public:
-    int hammingWeight(uint32_t n) {
-        /*int count = 0;
-        while (n != 0)
-        {
-            count += (n % 2);
-            n = n >> 1;
-        }
-        return count;*/ // my solution
-        int count = 0;
-        vector<int> v;
-        while(n / 2 != 0) {
-            v.push_back(n % 2);
-            n /= 2;
-        }
-        v.push_back(n % 2);
-        
-        for(const auto& x : v)
-            if(x == 1) 
-                count++;
-        return count;
+    int hammingWeight(int n) {
+        if(n == 0)
+            return 0;
+        else if(n % 2 == 0)
+            return hammingWeight(n / 2);
+        else
+            return 1 + hammingWeight(n / 2);
     }
 };
